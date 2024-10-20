56761ad10f8cfe9276ad08940afa863aafb33b1c
Carl Ericsson <carl.ericsson@gmail.com>
2024-10-20 08:21:30 +0200

    Made the scheduled task interval configurable

--- stores/src/main/java/com/careri78/stores/app/scheduling/OutboxPublishTask.java
+++ stores/src/main/java/com/careri78/stores/app/scheduling/OutboxPublishTask.java
@@ -4,23 +4,39 @@ import java.util.concurrent.ExecutionException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.stereotype.Component;
 
 import com.careri78.stores.core.services.OutboxPublishService;
 
+import java.time.Duration;
+
+/**
+ * If for some reason the outbox publish
+ * isn't triggered at once on a new event. 
+ * This scheduled task runs at a dynamically configured interval.
+ * 
+ * @author Carl Ericsson
+ * 
+ */
 @Component
 public final class OutboxPublishTask {
     private static final Logger log = LoggerFactory.getLogger(OutboxPublishTask.class);
     private final OutboxPublishService service;
+    int sendSchedule;
 
-    public OutboxPublishTask(final OutboxPublishService service) {
+    public OutboxPublishTask(
+        final OutboxPublishService service,
+        final TaskScheduler taskScheduler,
+        @Value("${outbox.send.schedule.seconds}") final int sendSchedule) {
         super();
         this.service = service;
+        this.sendSchedule = sendSchedule;
+        taskScheduler.scheduleWithFixedDelay(() -> triggerSend(), Duration.ofSeconds(sendSchedule));
     }
 
-	@Scheduled(fixedDelay = 5000)
-	public void reportCurrentTime() {
+	public void triggerSend() {
 		try {
             log.debug("Trigger publish all");
             service.publishAllAsync().get();
